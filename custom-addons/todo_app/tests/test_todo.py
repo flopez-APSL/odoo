47d3a39c1bb8f7622ca9dc74c66817a99ff2cbd1
flopez <cainuriel@gmail.com>
2019-03-19 14:54:46 +0100

    Falla el test record_rule. Añadiendo controladores web

--- custom-addons/todo_app/tests/test_todo.py
+++ custom-addons/todo_app/tests/test_todo.py
@@ -1,4 +1,5 @@
 from odoo.tests.common import TransactionCase
+from odoo.exceptions import AccessError
 
 
 class TestTodo(TransactionCase):
@@ -11,11 +12,28 @@ class TestTodo(TransactionCase):
         task = Todo.create({'name': 'Test Task'})
         self.assertEqual(task.is_done, False)
 
-    def test_clear_done(self):  # comprueba que esté el atributo 'is done' por defecto en False.
+    def test_clear_done(self):
         Todo = self.env['todo.task']
         task = Todo.create({'name': 'Test Task'})
         task.do_clear_done()
-        self.assertEqual(task.active)
+        self.assertFalse(task.active)
+
+    def setUp(self, *args, **kwargs):
+        result = super(TestTodo, self).setUp(*args, **kwargs)
+        user_demo = self.env.ref('base.user_demo')
+        self.env= self.env(user=user_demo)
+        return result
+
+    def test_record_rule(self):
+        """Test for Record Rules"""
+        Todo = self.env('todo.task')
+        task = Todo.sudo().create({'name': 'Admin Task'})
+        with self.assertRaises(AccessError):
+            Todo.browse([task.id]).name
+
+
+
+
 
 
 
