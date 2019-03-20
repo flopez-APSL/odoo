95599a8ed96ab0ff1d11da1ce08f57775651ff93
flopez <cainuriel@gmail.com>
2019-03-20 16:23:59 +0100

    Nuevo modulo openacademy

--- custom-addons/todo_app/controllers/main.py
+++ custom-addons/todo_app/controllers/main.py
@@ -6,7 +6,7 @@ class Todo(http.Controller):
     @http.route('/todo')
     def Main(self, **kwargs):
         TodoTask = http.request.env['todo.task']
-        domain_todo = [('its_done', '=', False)]
+        domain_todo = [('is_done', '=', False)]
         tasks = TodoTask.search(domain_todo)
         return http.request.render(
             'todo_app.index_template', {'tasks': tasks}
