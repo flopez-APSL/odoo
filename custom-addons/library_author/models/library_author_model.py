9eb306ceb4d30a8c182631e841f83bd9050e05f5
flopez <cainuriel@gmail.com>
2019-03-26 17:19:17 +0100

    Colores en el menu arbol de library_author

--- custom-addons/library_author/models/library_author_model.py
+++ custom-addons/library_author/models/library_author_model.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class LibraryAuthor(models.Model):
@@ -8,3 +8,15 @@ class LibraryAuthor(models.Model):
     active = fields.Boolean(default=True)
     country_id = fields.Many2one('res.country')
     # country_image = fields.Binary(related='country_id.image')  intento para coger un único campo de otro modelo.
+
+    book_ids = fields.Many2many('library.book', string="Books")
+
+    count_books = fields.Integer(compute='total_books', string="Total Books")
+
+    @api.depends('book_ids')
+    def total_books(self):
+        for record in self:
+            record.count_books = len(record.book_ids)
+
+
+
